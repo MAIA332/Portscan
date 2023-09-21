1fed896187dda82dbcd35e338b984b721ad1c869
MAIA332 <lk.maia0505@outlook.com>
2023-09-21 16:04:42 -0300

    clas update

--- _scn_.py
+++ _scn_.py
@@ -2,9 +2,12 @@ import socket
 
 class scan:
 
-    def listen_specify(self, s):
-        ip = input('IPV4: ')
-        port = int(input('PORT: '))
+    def __init__(self) -> None:
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.hostname = socket.gethostname()
+        self.ip_host = socket.gethostbyname(self.hostname)
+
+    def listen_specify(self, s,ip,port):
 
         while True:
             if s.connect_ex((ip,port)):
@@ -17,52 +20,25 @@ class scan:
                 break
 
 
-    def verify(self,s):
-        ip = input('IPV4: ')
-        ports = []
+    def verify(self,s,ip,ports):
+        log ={
 
-        
-        while True:
-            try:
-                port = int(input('PORT: '))
-
-                if port == 0000:
-                    break
-
-                ports.append(port)
-                
-                if(len(ports) == 10):
-                    print('Limite de portas atingido')
-                    break
-            except:
-                break
+        }
             
         for i in ports:
             s.settimeout(1.0)
             res = s.connect_ex((ip,i))
 
             if res == 0:
+                log[i]=True
                 print(f'Porta {i} do host {ip} está aberta')
 
             else:
+                log[i]=False
                 print(f'Porta {i} do host {ip} está fechada')
-    
 
+        return log
 
-
-    def Main(self,):
-
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
-        print("========================================")
-        print("========================================")
-        print("=== |Escutar Portas especificas (1)| ===\n=== |Verificar portas abertas (2)| ===")
-
-            
-        func = int(input('Scan-: '))
-
-        if func == 1:
-            self.listen_specify(s)
-
-        elif func == 2:
-            self.verify(s)
+        
+        
