71e47c4840ef59451489f96c602c19623dc3c209
MAIA332 <lk.maia0505@outlook.com>
2023-09-22 08:27:22 -0300

    service name + PID

--- _scn_.py
+++ _scn_.py
@@ -1,13 +1,13 @@
 class scan:
 
     def __init__(self) -> None:
-        import socket
-        import os
+        self.socket = __import__("socket")
+        self.os = __import__("os")
+        self.psutil = __import__("psutil")
 
-        self.os = os
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.hostname = socket.gethostname()
-        self.ip_host = socket.gethostbyname(self.hostname)
+        self.s = self.socket.socket(self.socket.AF_INET, self.socket.SOCK_STREAM)
+        self.hostname = self.socket.gethostname()
+        self.ip_host = self.socket.gethostbyname(self.hostname)
 
     def listen_specify(self, s,ip,port):
 
@@ -20,33 +20,41 @@ class scan:
                 break
 
     def verify_one(self,s,ip,port): # Verifica se uma porta está em uso
-        
         try:
-            if s.connect((ip,port)):
-                print(f"socket pluged on {port}")
-                s.close()
-                return True
-            else:
-                print(f"A porta {port} está em uso")
-                s.close()
-                return False
-            
-        except Exception as e:
-            print(f"A porta {port} está em uso, há uma aplicação rodando...")
+            s.settimeout(1)
+            # Tenta se conectar à porta, se ele conseguir se conectar na porta é por que alguma outra aplicação já a abriu
+            s.connect((ip, port))
             s.close()
+            return True
+        except (self.socket.timeout, ConnectionRefusedError):
             return False
 
-    def verify_many(self,s,ip,ports): # Verifica estado das portas de aplicação
-        log ={
 
-        }
-            
-        for i in ports:
-            a=self.verify_one(s,ip,i)
-            log[i] = a
-
-        return log
-
+    def get_service_name(self,s,port):
+        # Obtém o nome do serviço associado à porta
+        try:
+            service_name = self.socket.getservbyport(port)
+            return service_name
+        except OSError as e:
+            return f"Serviço desconhecido: {e}"
         
+    def get_pid_using_port(self,port):
+        # Obtém o PID associado à porta
+        for conn in self.psutil.net_connections():
+            if conn.laddr.port == port:
+                return conn.pid
+        return None
+
+    def terminate_process(self,pid):
+        try:
+            process = self.psutil.Process(pid)
+            process.terminate()
+            print(f"Processo com PID {pid} terminado com sucesso.")
+        except self.psutil.NoSuchProcess:
+            print(f"O processo com PID {pid} não foi encontrado.")
+        except self.psutil.AccessDenied:
+            print(f"Não foi possível encerrar o processo com PID {pid}. Permissão negada.")
+
+            
         
         
